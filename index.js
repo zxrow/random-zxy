ca948b304027fabce743e857f90c9a1e159c917d
Z-zxQ <49530313+Z-zxQ@users.noreply.github.com>
2021-07-14 06:34:33 +0700

    index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function anunya(len) {
 }
 
 console.log(chalk.cyan(`${chalk.red('- - OTAKU | ANIMLOVER - -')}\nRandom Zxy\nCreated by zxagung\nFb : facebook.com/laoneisagung9\nIG : instagram.com/_zxagung\n${chalk.red('- - SANSEKAI - -')}\n`))
-console.log(`${chalk.cyan('TYPE')}\n1. Wallpaper\n2. Waifu\n3. Waifu (NSFW)\n4. neko\n5. trap\n6. blowjob\n7. asupan tiktok video\n8. meme spongbob\n9. random gambar 18\n10. random loli gif\n11. pemandangan anime\n12. pemandangan animev2\n`)
+console.log(`${chalk.cyan('TYPE')}\n1. Wallpaper\n2. Waifu\n3. Waifu (NSFW)\n4. neko\n5. trap\n6. blowjob\n7. asupan tiktok video\n8. meme spongbob\n9. random gambar 18\n10. random loli gif\n11. pemandangan anime\n12. pemandangan animev2\n13. random nhentai\n`)
 
 const type = readlineSync.questionInt(chalk.yellow("- Mau type nomor berapa?: "))
-if (type > 12) return console.log('Pilihannya cuma 1 - 12 gan')
+if (type > 13) return console.log('Pilihannya cuma 1 - 13 gan')
 
 const jml = readlineSync.questionInt(chalk.yellow("- Mau download berapa? (max 50): "))
-if (jml > 50) return console.log('Maksimal 50 gan')
+if (jml > 10) return console.log('Maksimal 10 gan')
 
 
 try {
@@ -229,8 +229,25 @@ try {
                             console.log(chalk.green('finished downloading!')))
                     }
                 })
+                } else if (type == '13') {
+            axios.get(`https://apixxy.herokuapp.com/api/randomnhentai?apikey=premiumzx`)
+                .then(async (res) => {
+                    const data = res.data.result.url
+                    if (fs.existsSync('/sdcard/wp-randomzxy')) {
+                        const response = await fetch(data);
+                        const buffer = await response.buffer();
+                        fs.writeFile(`/sdcard/wp-randomzxy/${anunya(5)}.jpg`, buffer, () =>
+                            console.log(chalk.green('finished downloading!')))
+                    } else {
+                        fs.mkdir('/sdcard/wp-randomzxy')
+                        const response = await fetch(data);
+                        const buffer = await response.buffer();
+                        fs.writeFile(`/sdcard/wp-randomzxy/${anunya(5)}.jpg`, buffer, () =>
+                            console.log(chalk.green('finished downloading!')))
+                    }
+                })
         } else {
-            console.log(chalk.red('Pilihan hanya 1 - 12'))
+            console.log(chalk.red('Pilihan hanya 1 - 13'))
         }
     }
 } catch (err) {
